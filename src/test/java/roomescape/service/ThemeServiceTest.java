36623f9f82bbf408d34f050e670a16db65518db3
YEJIN KIM <glow.yejin@gmail.com>
2026-05-19 17:36:12 +0900

    test: ThemeServiceTest 불필요한 Clock 의존성 제거

--- src/test/java/roomescape/service/ThemeServiceTest.java
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -3,7 +3,6 @@ package roomescape.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -30,8 +29,6 @@ import roomescape.repository.ThemeRepository;
 class ThemeServiceTest {
 
     @Autowired
-    Clock clock;
-    @Autowired
     private ThemeService themeService;
     @Autowired
     private ThemeRepository themeRepository;
@@ -47,10 +44,8 @@ class ThemeServiceTest {
         List<ThemeResponseDTO> popularThemes = themeService.getPopularThemes(1L, 10L);
 
         // then
-        assertThat(popularThemes)
-                .map(ThemeResponseDTO::id)
-                .containsExactly(
-                        1L, 2L, 3L, // 1순위: 테마의 예약 수 내림차순 정렬
+        assertThat(popularThemes).map(ThemeResponseDTO::id)
+                .containsExactly(1L, 2L, 3L, // 1순위: 테마의 예약 수 내림차순 정렬
                         6L, 5L, 4L, 8L, 7L, // 2순위: 예약 수가 같으면 테마 이름 오름차순 정렬
                         10L, 9L // 예약 개수가 0개여도, 상위 10위 이내라면 조회되어야 함 (예약 개수 0개인 테마들은 2순위 정렬 기준으로 비교)
                 );
@@ -59,17 +54,12 @@ class ThemeServiceTest {
     @Test
     void 중복된_테마를_추가하면_예외가_발생한다() {
         // given
-        ThemeRequestDTO request = new ThemeRequestDTO(
-                "귀신찾기",
-                "귀신을 찾는다",
-                "https://image.png"
-        );
+        ThemeRequestDTO request = new ThemeRequestDTO("귀신찾기", "귀신을 찾는다", "https://image.png");
         themeService.addTheme(request);
 
         // when & then
-        assertThatThrownBy(() -> themeService.addTheme(request))
-                .isInstanceOf(RoomEscapeException.class)
-                .extracting("errorCode")
+        assertThatThrownBy(() -> themeService.addTheme(request)).isInstanceOf(
+                        RoomEscapeException.class).extracting("errorCode")
                 .isEqualTo(ThemeErrorCode.THEME_DUPLICATE);
     }
 
@@ -77,10 +67,8 @@ class ThemeServiceTest {
     @Test
     void 존재하지_않는_테마를_조회하면_예외가_발생한다() {
         // when & then
-        assertThatThrownBy(() -> themeService.findById(1L))
-                .isInstanceOf(RoomEscapeException.class)
-                .extracting("errorCode")
-                .isEqualTo(ThemeErrorCode.THEME_NOT_FOUND);
+        assertThatThrownBy(() -> themeService.findById(1L)).isInstanceOf(RoomEscapeException.class)
+                .extracting("errorCode").isEqualTo(ThemeErrorCode.THEME_NOT_FOUND);
     }
 
 
@@ -88,19 +76,14 @@ class ThemeServiceTest {
     void 예약이_존재하는_테마를_삭제하면_예외가_발생한다() {
         // given
         ReservationTime time = reservationTimeRepository.save(
-                ReservationTime.create(LocalTime.parse("10:00"))
-        );
-        Theme theme = themeRepository.save(
-                Theme.create("귀신찾기", "귀신을 찾는다", "https://image.png")
-        );
+                ReservationTime.create(LocalTime.parse("10:00")));
+        Theme theme = themeRepository.save(Theme.create("귀신찾기", "귀신을 찾는다", "https://image.png"));
         reservationRepository.save(
-                Reservation.create("브라운", LocalDate.parse("2026-08-05"), time, theme)
-        );
+                Reservation.create("브라운", LocalDate.parse("2026-08-05"), time, theme));
 
         // when & then
-        assertThatThrownBy(() -> themeService.deleteTheme(theme.getId()))
-                .isInstanceOf(RoomEscapeException.class)
-                .extracting("errorCode")
+        assertThatThrownBy(() -> themeService.deleteTheme(theme.getId())).isInstanceOf(
+                        RoomEscapeException.class).extracting("errorCode")
                 .isEqualTo(ThemeErrorCode.RESERVATION_EXIST_ON_THEME);
     }
 }
